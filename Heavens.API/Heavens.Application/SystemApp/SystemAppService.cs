023b192820e98bd9f35418d8aa5c45aa0163df7b
yi zhu <1047606598@qq.com>
2022-02-27 17:19:15 +0800

    添加获取后台版本信息接口

---
+++ Heavens.API/Heavens.Application/SystemApp/SystemAppService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heavens.Application.SystemApp;
+[AllowAnonymous]
+public class SystemAppService : IDynamicApiController
+{
+
+    public string version { get; set; }
+
+    /// <summary>
+    /// 获取后台版本号
+    /// </summary>
+    /// <returns></returns>
+    public string GetVersion()
+    {
+        if (version.IsEmpty())
+        {
+            Version v = Assembly.GetEntryAssembly().GetFileVersion();
+            version = v.ToString();
+        }
+        return version;
+    }
+}

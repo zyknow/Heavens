437302913edb3c82b2cec59b277aee11a9c08356
yi zhu <1047606598@qq.com>
2022-03-08 15:28:22 +0800

    QueryFilter单元测试

---
+++ Heavens.API/Heavens.Tests/Core/QueayFilter/Models/Grade.cs
@@ -0,0 +1,20 @@
+﻿using Heavens.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heavens.Core.Entities;
+/// <summary>
+/// QueryFilter 测试实体
+/// </summary>
+public class Grade
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public DateTimeOffset OffsetTime { get; set; }
+
+    public DateTime Time { get; set; }
+}
